22c9be44df0b3c6c23c7b1d542efd776c972eeb0
AishwaryaHalve8 <aishwaryahalve@gmail.com>
2015-09-02 00:10:34 -0700

    student and faculty login php

---
+++ student_login.php
@@ -0,0 +1,23 @@
+<?php
+$hostname_localhost ="localhost";
+$database_localhost ="soc";
+$username_localhost ="root";
+$password_localhost ="";
+$localhost = mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost)
+or
+trigger_error(mysql_error(),E_USER_ERROR);
+ 
+ 
+$username = $_POST['student_name'];
+$password = $_POST['student_pass'];
+$query_search = "select * from student where name = '".$username."' AND password = '".$password. "'";
+$query_exec = mysqli_query($localhost, $query_search);
+$rows = mysqli_num_rows($query_exec);
+//echo $rows;
+ if($rows == 0) { 
+ echo "No Such User Found"; 
+ }
+ else  {
+    echo "User Found"; 
+}
+?>
