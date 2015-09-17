dae32fd1b084b9ce3752687c30eb6b448c041f90
AishwaryaHalve8 <aishwaryahalve@gmail.com>
2015-09-17 10:09:36 -0700

    php changes

--- noti_fire.php
+++ noti_fire.php
@@ -9,13 +9,15 @@ trigger_error(mysql_error(),E_USER_ERROR);
  
  
 $subject = $_POST['subject'];
+date_default_timezone_set('Asia/Calcutta');
 $link = $_POST['link'];
-$des=$_POST['discription'];
+$date = date('m/d/Y h:i:s a', time());
+$des=$_POST['des'];
 $name=$_POST['name'];
-$query_search = "INSERT INTO notification (description, link, topic, username) VALUES ('".$des."','".$link."','".$subject."','".$name."')";
+$query_search = "INSERT INTO notification (description, link, topic, username,date) VALUES ('$_POST[des]','$_POST[link]','$_POST[subject]','$_POST[name]','$date')";
 $query_exec = mysqli_query($localhost, $query_search);
 if ($query_exec) {
     echo "New notification created successfully";
 } else {
-    echo "Error: " . $query_search . "<br>" . mysqli_error($conn);
+    echo "Error: " . $query_search . "<br>" . mysqli_error($localhost);
 }
